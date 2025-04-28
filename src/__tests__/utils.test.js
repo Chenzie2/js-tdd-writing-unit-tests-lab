ea11d3f52dabdeb45d4a1722c97c5cdb8745dfea
g_zawie <gzawie24@gmail.com>
2025-04-28 07:47:01 +0300

    unit test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
 // Your tests here
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome', () => {
+  test('should return true for a palindrome word', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  test('should return false for a non-palindrome word', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+  test('should return true for a palindrome word with mixed case', () => {
+    expect(isPalindrome('RaceCar')).toBe(true);
+  });
+
+  test('should return false for an empty string', () => {
+    expect(isPalindrome('')).toBe(false);
+  });
+
+  test('should throw an error if the input is not a string', () => {
+    expect(() => isPalindrome(123)).toThrow('Input must be a string');
+  });
+
+  test('should throw an error if the string contains non-alphabetic characters', () => {
+    expect(() => isPalindrome('racecar!')).toThrow('Input must only contain alphabetic characters');
+  });
+});
